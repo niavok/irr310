562d777d0bd21858bd7d425581592c6304aaaf04
Frédéric Bertolus <fred@elveos.org>
2011-10-27 12:31:01 +0200

    fix collision bug and add fixed framerate

--- src/com/irr310/server/PhysicEngine.java
+++ src/com/irr310/server/PhysicEngine.java
@@ -48,7 +48,7 @@ import com.irr310.server.game.world.WorldObject;
 public class PhysicEngine extends Engine {
 
 	public static final float PI_2 = 1.57079632679489661923f;
-	
+
 	// keep the collision shapes, for deletion/cleanup
 	private ObjectArrayList<CollisionShape> collisionShapes = new ObjectArrayList<CollisionShape>();
 	private BroadphaseInterface broadphase;
@@ -56,99 +56,103 @@ public class PhysicEngine extends Engine {
 	private ConstraintSolver solver;
 	private DefaultCollisionConfiguration collisionConfiguration;
 	protected Clock clock = new Clock();
-	
+
 	// this is the most important class
 	protected DynamicsWorld dynamicsWorld = null;
 	private Map<Part, RigidBody> partToBodyMap;
 
-	
 	public PhysicEngine() {
-		framerate = new Duration(10000000);
-		
+		framerate = new Duration(10000000); // 10 ms
+
 		initPhysics();
 	}
 
 	@Override
 	protected void frame() {
-		// simple dynamics world doesn't handle fixed-time-stepping
-		float ms = getDeltaTimeMicroseconds();
 
 		// step the simulation
 		if (dynamicsWorld != null) {
-			dynamicsWorld.stepSimulation(ms / 1000000f);
+			dynamicsWorld.stepSimulation(framerate.getSeconds());
 		}
-		
+
 	}
-	
-	
+
 	public float getDeltaTimeMicroseconds() {
 		float dt = clock.getTimeMicroseconds();
 		clock.reset();
 		return dt;
 	}
-	
+
 	public void initPhysics() {
 
 		partToBodyMap = new HashMap<Part, RigidBody>();
-		
-		// collision configuration contains default setup for memory, collision setup
+
+		// collision configuration contains default setup for memory, collision
+		// setup
 		collisionConfiguration = new DefaultCollisionConfiguration();
 
-		// use the default collision dispatcher. For parallel processing you can use a diffent dispatcher (see Extras/BulletMultiThreaded)
+		// use the default collision dispatcher. For parallel processing you can
+		// use a diffent dispatcher (see Extras/BulletMultiThreaded)
 		dispatcher = new CollisionDispatcher(collisionConfiguration);
 
 		broadphase = new DbvtBroadphase();
 
-		// the default constraint solver. For parallel processing you can use a different solver (see Extras/BulletMultiThreaded)
+		// the default constraint solver. For parallel processing you can use a
+		// different solver (see Extras/BulletMultiThreaded)
 		SequentialImpulseConstraintSolver sol = new SequentialImpulseConstraintSolver();
 		solver = sol;
-		
+
 		// TODO: needed for SimpleDynamicsWorld
-		//sol.setSolverMode(sol.getSolverMode() & ~SolverMode.SOLVER_CACHE_FRIENDLY.getMask());
-		
-		dynamicsWorld = new DiscreteDynamicsWorld(dispatcher, broadphase, solver, collisionConfiguration);
+		// sol.setSolverMode(sol.getSolverMode() &
+		// ~SolverMode.SOLVER_CACHE_FRIENDLY.getMask());
+
+		dynamicsWorld = new DiscreteDynamicsWorld(dispatcher, broadphase,
+				solver, collisionConfiguration);
 
 		// No gravity
 		dynamicsWorld.setGravity(new Vector3f(0f, 0f, 0f));
 
-		
-		dynamicsWorld.getPairCache().setOverlapFilterCallback(new OverlapFilterCallback() {
-			
-			@Override
-			public boolean needBroadphaseCollision(BroadphaseProxy proxy0,
-					BroadphaseProxy proxy1) {
-				
-				
-				UserData data0 = (UserData)((RigidBody) proxy0.clientObject).getUserPointer();
-				UserData data1 = (UserData)((RigidBody) proxy0.clientObject).getUserPointer();
-				
-				if(data0 != null && data1 != null && data0.ship == data1.ship) {
-					return false;
-				}
-				
-				return true;
-			}
-		});
+		dynamicsWorld.getPairCache().setOverlapFilterCallback(
+				new OverlapFilterCallback() {
 
-		
+					@Override
+					public boolean needBroadphaseCollision(
+							BroadphaseProxy proxy0, BroadphaseProxy proxy1) {
+
+						UserData data0 = (UserData) ((RigidBody) proxy0.clientObject)
+								.getUserPointer();
+						UserData data1 = (UserData) ((RigidBody) proxy1.clientObject)
+								.getUserPointer();
+
+						// System.out.println("test collision");
+
+						if (data0 != null && data1 != null
+								&& data0.ship == data1.ship) {
+							// System.out.println("cancel collision for "+data0.ship);
+							return false;
+						}
+
+						// System.out.println("valid collision");
+						return true;
+					}
+				});
 
 	}
-	
-	
-		
-	
+
 	protected void addObject(WorldObject object) {
-		for(final Part part: object.getParts()) {
+		for (final Part part : object.getParts()) {
 			addPart(part);
 		}
 	}
-	
-	protected void addShip(Ship ship) {
-		
-		
-		
-		for(Component component:  ship.getComponents()) {
-			for(final Part part: component.getParts()) {
+
+	protected void addShip(Ship ship, Vect3 position) {
+
+		for (Component component : ship.getComponents()) {
+			for (final Part part : component.getParts()) {
+
+				part.getTransform().setTranslation(
+						position.plus(component.getShipPosition()));
+
 				RigidBody rigidBody = addPart(part);
 				partToBodyMap.put(part, rigidBody);
 				UserData userData = new UserData();
@@ -156,62 +160,70 @@ public class PhysicEngine extends Engine {
 				rigidBody.setUserPointer(userData);
 			}
 		}
-		
-		for(Link link: ship.getLinks()) {
+
+		for (Link link : ship.getLinks()) {
 			addLink(link);
 		}
 	}
-	
-	protected  void addLink(Link link) {
+
+	protected void addLink(Link link) {
 		Slot slot1 = link.getSlot1();
 		Slot slot2 = link.getSlot2();
 		RigidBody body1 = partToBodyMap.get(slot1.getPart());
 		RigidBody body2 = partToBodyMap.get(link.getSlot2().getPart());
-		
-		
+
 		{
 			Transform localA = new Transform(), localB = new Transform();
 			localA.setIdentity();
 			localB.setIdentity();
 			MatrixUtil.setEulerZYX(localA.basis, 0, PI_2, 0);
-			localA.origin.set(slot1.getPosition().x.floatValue(),slot1.getPosition().y.floatValue(), slot1.getPosition().z.floatValue());
+			localA.origin.set(slot1.getPosition().x.floatValue(),
+					slot1.getPosition().y.floatValue(),
+					slot1.getPosition().z.floatValue());
 			MatrixUtil.setEulerZYX(localB.basis, 0, PI_2, 0);
-			localB.origin.set(slot2.getPosition().x.floatValue(),slot2.getPosition().y.floatValue(), slot2.getPosition().z.floatValue());
-			HingeConstraint constraint = new HingeConstraint(body1, body2, localA, localB);
+			localB.origin.set(slot2.getPosition().x.floatValue(),
+					slot2.getPosition().y.floatValue(),
+					slot2.getPosition().z.floatValue());
+			HingeConstraint constraint = new HingeConstraint(body1, body2,
+					localA, localB);
 			dynamicsWorld.addConstraint(constraint, true);
 		}
-		
+
 		{
 			Transform localA = new Transform(), localB = new Transform();
 			localA.setIdentity();
 			localB.setIdentity();
 			MatrixUtil.setEulerZYX(localA.basis, PI_2, 0, 0);
-			localA.origin.set(slot1.getPosition().x.floatValue(),slot1.getPosition().y.floatValue(), slot1.getPosition().z.floatValue());
+			localA.origin.set(slot1.getPosition().x.floatValue(),
+					slot1.getPosition().y.floatValue(),
+					slot1.getPosition().z.floatValue());
 			MatrixUtil.setEulerZYX(localB.basis, PI_2, 0, 0);
-			localB.origin.set(slot2.getPosition().x.floatValue(),slot2.getPosition().y.floatValue(), slot2.getPosition().z.floatValue());
-			HingeConstraint constraint = new HingeConstraint(body1, body2, localA, localB);
+			localB.origin.set(slot2.getPosition().x.floatValue(),
+					slot2.getPosition().y.floatValue(),
+					slot2.getPosition().z.floatValue());
+			HingeConstraint constraint = new HingeConstraint(body1, body2,
+					localA, localB);
 			dynamicsWorld.addConstraint(constraint, true);
 		}
-		
-		
+
 	}
-	
+
 	protected RigidBody addPart(Part part) {
 		// create a few dynamic rigidbodies
-		// Re-using the same collision is better for memory usage and performance
+		// Re-using the same collision is better for memory usage and
+		// performance
 
-		
-		
-		CollisionShape colShape = new BoxShape(part.getShape().divide(2).toVector3f());
-		//CollisionShape colShape = new SphereShape(1f);
+		CollisionShape colShape = new BoxShape(part.getShape().divide(2)
+				.toVector3f());
+		// CollisionShape colShape = new SphereShape(1f);
 		collisionShapes.add(colShape);
 
 		// Create Dynamic Objects
-		
 
 		float mass = part.getMass().floatValue();
 
-		// rigidbody is dynamic if and only if mass is non zero, otherwise static
+		// rigidbody is dynamic if and only if mass is non zero, otherwise
+		// static
 		boolean isDynamic = (mass != 0f);
 
 		Vector3f localInertia = new Vector3f(0, 0, 0);
@@ -219,37 +231,39 @@ public class PhysicEngine extends Engine {
 			colShape.calculateLocalInertia(mass, localInertia);
 		}
 
-		
-		//TODO rotation
+		// TODO rotation
 
-		// using motionstate is recommended, it provides interpolation capabilities, and only synchronizes 'active' objects
+		// using motionstate is recommended, it provides interpolation
+		// capabilities, and only synchronizes 'active' objects
 		PartMotionState myMotionState = new PartMotionState(part);
-		RigidBodyConstructionInfo rbInfo = new RigidBodyConstructionInfo(mass, myMotionState, colShape, localInertia);
+		RigidBodyConstructionInfo rbInfo = new RigidBodyConstructionInfo(mass,
+				myMotionState, colShape, localInertia);
 		RigidBody body = new RigidBody(rbInfo);
-		
+
 		myMotionState.setBody(body);
-		
+
 		body.setActivationState(RigidBody.ISLAND_SLEEPING);
 		body.setDamping(0.001f, 0.1f);
 		body.setSleepingThresholds(0.001f, 0.001f);
-		//body.setDeactivationTime(deactivationTime)
+		// body.setDeactivationTime(deactivationTime)
 
 		dynamicsWorld.addRigidBody(body);
-		
+
 		body.setLinearVelocity(part.getLinearSpeed().toVector3f());
 		body.setAngularVelocity(part.getRotationSpeed().toVector3f());
 		body.setActivationState(RigidBody.ACTIVE_TAG);
-		
+		body.setCcdMotionThreshold(1f);
+		body.setCcdSweptSphereRadius(0.2f);
 		return body;
-		
+
 	}
-	
+
 	public class UserData {
-		
+
 		public Ship ship = null;
-		
+
 	}
-	
+
 	public class PartMotionState extends MotionState {
 
 		private final Part part;
@@ -257,9 +271,9 @@ public class PhysicEngine extends Engine {
 
 		PartMotionState(Part part) {
 			this.part = part;
-			
+
 		}
-		
+
 		public void setBody(RigidBody body) {
 			this.body = body;
 		}
@@ -275,37 +289,31 @@ public class PhysicEngine extends Engine {
 		public void setWorldTransform(Transform worldTrans) {
 			Vector3f origin = worldTrans.origin;
 			part.getTransform().setTranslation(origin.x, origin.y, origin.z);
-			//Vector3f linearVelocity = body.getLinearVelocity(new Vector3f());
+			// Vector3f linearVelocity = body.getLinearVelocity(new Vector3f());
 			worldTrans.getOpenGLMatrix(part.getTransform().getData());
 			part.getTransform().fireChanged();
-			Quat4f rotation = worldTrans.getRotation(new Quat4f());
-			
-			//System.out.println("w="+rotation.w+" x="+rotation.x+" y="+rotation.y+" z="+rotation.z);
-			
-			//System.out.println("x="+origin.x+" y="+origin.y+" z="+origin.z+" vx="+linearVelocity.x+" vy="+linearVelocity.y+" vz="+linearVelocity.z+ " desactivation_time="+body.getDeactivationTime()+""+body.getLinearSleepingThreshold());
-		}		
-	}
-	
-	/*class ShipFilterCallback : public btOverlapFilterCallback
-	{
-		// return true when pairs need collision
-		virtual bool	needBroadphaseCollision(btBroadphaseProxy* proxy0,btBroadphaseProxy* proxy1) const
-		{
-			bool collides = (proxy0->m_collisionFilterGroup & proxy1->m_collisionFilterMask) != 0;
-			collides = collides && (proxy1->m_collisionFilterGroup & proxy0->m_collisionFilterMask);
-			
-			//add some additional logic here that modified 'collides'
-			return collides;
 		}
-	};*/
-	
+	}
+
+	/*
+	 * class ShipFilterCallback : public btOverlapFilterCallback { // return
+	 * true when pairs need collision virtual bool
+	 * needBroadphaseCollision(btBroadphaseProxy* proxy0,btBroadphaseProxy*
+	 * proxy1) const { bool collides = (proxy0->m_collisionFilterGroup &
+	 * proxy1->m_collisionFilterMask) != 0; collides = collides &&
+	 * (proxy1->m_collisionFilterGroup & proxy0->m_collisionFilterMask);
+	 * 
+	 * //add some additional logic here that modified 'collides' return
+	 * collides; } };
+	 */
 
 	@Override
 	protected void processEvent(EngineEvent e) {
 		e.accept(new PhysicEngineEventVisitor());
 	}
 
-	private final class PhysicEngineEventVisitor extends DefaultEngineEventVisitor {
+	private final class PhysicEngineEventVisitor extends
+			DefaultEngineEventVisitor {
 		@Override
 		public void visit(QuitGameEvent event) {
 			System.out.println("stopping physic engine");
@@ -327,27 +335,24 @@ public class PhysicEngine extends Engine {
 		public void visit(WorldObjectAddedEvent event) {
 			addObject(event.getObject());
 		}
-		
-		
+
 		@Override
 		public void visit(WorldShipAddedEvent event) {
-			addShip(event.getShip());
+			addShip(event.getShip(), event.getPosition());
 		}
-		
-		
-		
+
 	}
 
 	@Override
 	protected void init() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	protected void end() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
