2704ff1af8f83cd5454f4836d0d386d810934d8f
Frédéric Bertolus <fred@elveos.org>
2011-11-01 00:24:38 +0100

    fix type conversion

--- src/com/irr310/common/network/generator/DoubleMessageFieldGenerator.java
+++ src/com/irr310/common/network/generator/DoubleMessageFieldGenerator.java
@@ -25,5 +25,40 @@ public class DoubleMessageFieldGenerator extends MessageFieldGenerator<Double> {
         return new ImmutablePair<Integer, Double>(8, Double.longBitsToDouble(bits));
 
     }
+    
+    public static void main(String[] args) {
+        DoubleMessageFieldGenerator generator = new DoubleMessageFieldGenerator();
+        
+        double d = 48;
+        
+        
+        
+        byte[] b = new byte[8];
+        
+        generator.write(d, b, 0);
+        double d2 = generator.load(b, 0).getRight();
+        
+        long l = Double.doubleToLongBits(d);
+        double d3 = Double.longBitsToDouble(l);
+        
+        System.out.println(d2);
+        System.out.println(l);
+        System.out.println(d3);
+        
+        LongMessageFieldGenerator lGenerator = new LongMessageFieldGenerator();
+        
+        byte[] b2 = new byte[8];
+        
+        long l2 = 48;
+        lGenerator.write(l2, b2, 0);
+        long l3 = lGenerator.load(b2, 0).getRight();
+        
+        
+        lGenerator.write(l, b2, 0);
+        long l4 = lGenerator.load(b2, 0).getRight();
+        
+        System.out.println(l3);
+        System.out.println(l4);
+    }
 
 }
--- src/com/irr310/common/tools/TypeConversion.java
+++ src/com/irr310/common/tools/TypeConversion.java
@@ -1,49 +1,31 @@
 package com.irr310.common.tools;
 
+import java.nio.ByteBuffer;
+
 public class TypeConversion {
 
     public static byte[] intToByteArray(int value) {
         byte[] b = new byte[4];
-        for (int i = 0; i < 4; i++) {
-            int offset = (4 - 1 - i) * 8;
-            b[i] = (byte) ((value >>> offset) & 0xFF);
-        }
-        return b;
-    }
-    
-    
-    public static byte[] writeIntToByteArray(int value, byte[] b, int offset) {
-        for (int i = 0; i < 4; i++) {
-            int o = (4 - 1 - i) * 8;
-            b[i+offset] = (byte) ((value >>> o) & 0xFF);
-        }
-        return b;
-    }
-    
-    public static byte[] writeLongToByteArray(long value, byte[] b, int offset) {
-        for (int i = 0; i < 8; i++) {
-            int o = (8 - 1 - i) * 8;
-            b[i+offset] = (byte) ((value >>> o) & 0xFF);
-        }
+        ByteBuffer.wrap(b, 0, 4).putInt(value);
         return b;
     }
 
+    public static byte[] writeIntToByteArray(int value, byte[] b, int offset) {
+        ByteBuffer.wrap(b, offset, 4).putInt(value);
+        return b;
+    }
+
+    public static byte[] writeLongToByteArray(long value, byte[] b, int offset) {
+        ByteBuffer.wrap(b, offset, 8).putLong(value);
+        return b;
+    }
 
     public static int intFromByteArray(byte[] b, int offset) {
-        int value = 0;
-        for (int i = 0; i < 4; i++) {
-            int shift = (4 - 1 - i) * 8;
-            value += (b[i + offset] & 0x000000FF) << shift;
-        }
-        return value;
+        return ByteBuffer.wrap(b, offset, 4).getInt();
+
     }
-   
+
     public static long longFromByteArray(byte[] b, int offset) {
-        long value = 0;
-        for (int i = 0; i < 8; i++) {
-            long shift = (8 - 1 - i) * 8;
-            value += (b[i + offset] & 0x000000FF) << shift;
-        }
-        return value;
+        return ByteBuffer.wrap(b, offset, 8).getLong();
     }
 }
