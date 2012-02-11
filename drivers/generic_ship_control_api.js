483e941c8a29f8bbe6d6f4e5ef6a4ce69f9446fb
Frédéric Bertolus <fred@elveos.org>
2012-02-12 00:25:39 +0100

    begin to define generic ship control api

---
+++ drivers/generic_ship_control_api.js
@@ -0,0 +1,58 @@
+// Generic Ship  Control API
+
+function getPosition() {
+    //Return a vector of the position of the ship
+}
+
+function getRotation() {
+    //Return a vector of the world rotation of the ship
+}
+
+function getSpeed() {
+    //Return a vector of the world linear speed of the ship
+}
+
+function getAngularSpeed() {
+    //Return a vector of the world angular speed of the ship
+}
+
+function getSize() {
+
+}
+
+function referencePosition() {
+
+}
+
+
+function moveForward() {
+
+}
+
+function moveBack() {
+
+}
+
+function turnLeft() {
+
+}
+
+function turnRight() {
+
+}
+
+function turnUp() {
+
+}
+
+function turnBottom() {
+
+}
+
+function rotateCW() {
+
+}
+
+function rotateCCW() {
+
+}
