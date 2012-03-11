0393e5cce7e71f34df6ed878d9dd064ef7afc6b2
Frédéric Bertolus <fred.bertolus@gmail.com>
2012-03-11 11:25:24 +0100

    begin to add inventory

---
+++ src/com/irr310/common/world/view/ItemView.java
@@ -0,0 +1,14 @@
+package com.irr310.common.world.view;
+
+import com.irr310.common.network.NetworkClass;
+import com.irr310.common.network.NetworkField;
+
+@NetworkClass
+public class ItemView {
+
+    @NetworkField
+    public long id;
+   
+    @NetworkField
+    public String name;
+}
