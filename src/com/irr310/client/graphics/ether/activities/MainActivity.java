e39ae051b9774446ac657a4d266be3af6cdc2e3a
Frédéric Bertolus <fred.bertolus@gmail.com>
2012-09-07 19:32:45 +0200

    Change loading animation

--- src/com/irr310/client/graphics/ether/activities/MainActivity.java
+++ src/com/irr310/client/graphics/ether/activities/MainActivity.java
@@ -11,48 +11,66 @@ import com.irr310.server.Time;
 public class MainActivity extends Activity {
 
     private Triangle mobileLogoPart;
-	private Measure animationMesure;
-	private Time lastTime;
+    private Measure animationMesure;
+    private Time lastTime;
+    private boolean axisX;
+    private float lastOffset;
+    private float lastLastOffset;
 
-	@Override
+    @Override
     public void onCreate(Object objectBundle) {
         setContentView("main@layout/main");
-        
+
         mobileLogoPart = (Triangle) findViewById("logoRedPart@layout/main");
         animationMesure = new Measure(0, true);
-        lastTime = Time.now(false);
+
     }
 
-	@Override
+    @Override
     public void onStart() {
-        
+        lastTime = Time.now(false);
+        axisX = true;
+        lastOffset = 0;
+        lastLastOffset = 0;
     }
 
     @Override
     public void onPause() {
-        
+
     }
 
     public void onResume() {
-        
+
     }
 
     @Override
     protected void onUpdate(Time absTime, Time gameTime) {
         Duration duration = lastTime.durationTo(absTime);
 
-        //Log.trace("Update animatation after "+duration.getMilliseconds()+" ms ("+(1f/duration.getSeconds())+"fps)");
+        // Log.trace("Update animatation after "+duration.getMilliseconds()+" ms ("+(1f/duration.getSeconds())+"fps)");
         lastTime = absTime;
 
         Layout layout = mobileLogoPart.getLayout();
-        animationMesure.setValue((float) (25f/4.5f * (1+ Math.sin(absTime.getSeconds()*3))));
+        animationMesure.setValue((float) (25f / 2.2f * (1 + Math.sin(absTime.getSeconds() * 4))));
 
         float offset = layout.computeMesure(animationMesure);
 
-        //Log.trace("Offset: "+offset);
+        // Log.trace("Offset: "+offset);
+        if (axisX) {
+            layout.setOffsetX(offset);
+        } else {
+            layout.setOffsetY(offset);
+        }
 
-        layout.setOffsetX(offset);
-        layout.setOffsetY(offset);
+        if ((lastOffset - lastLastOffset) < 0 && (offset - lastOffset > 0)) {
+            // Cross a minimal
+            axisX = !axisX;
+        }
+
+        lastLastOffset = lastOffset;
+        lastOffset = offset;
+
+        // layout.setOffsetY(offset);
     }
 
 }
