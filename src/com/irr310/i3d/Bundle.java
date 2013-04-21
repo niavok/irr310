0e55571d5824a1d4dbcd61b543892faa3f94cac7
Frédéric Bertolus <fred.bertolus@gmail.com>
2013-04-21 17:07:16 +0200

    Implement hascode and equal in Bundle to fix activity stack bugs

--- src/com/irr310/i3d/Bundle.java
+++ src/com/irr310/i3d/Bundle.java
@@ -12,6 +12,28 @@ public class Bundle {
         return obj;
     }
 
-    
-    //TODO : generate equal && hashcode
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((obj == null) ? 0 : obj.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Bundle other = (Bundle) obj;
+        if (this.obj == null) {
+            if (other.obj != null)
+                return false;
+        } else if (!this.obj.equals(other.obj))
+            return false;
+        return true;
+    }
 }
